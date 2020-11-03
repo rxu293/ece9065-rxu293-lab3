c14bf1853cbe92cec1fa65a4d5c6fed2e6054d99
rxu293 <rxu293@uwo.ca>
2020-11-03 01:29:05 -0500

    added fetch funtions to get data from back-end for function 1 and 2

--- Node.js
+++ Node.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const port = 3000;
+let allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Headers', "*");
+  next();
+}
+app.use(allowCrossDomain);
 app.use(express.json());
 //setup the database
 const low = require('lowdb');
--- static/course.js
+++ static/course.js
@@ -1,2 +1,86 @@
-var p = document.getElementById("res");
+document.getElementById("get_all_courses_btn").onclick = function() {getAllCourses()}
+document.getElementById("get_codes_by_subject_btn").onclick = function() {getCodesByGivenSubject()}
 
+//fetch function 1
+function getAllCourses()
+{
+	fetch('http://localhost:3000/api/courses')
+	.then((res) => res.json())
+	.then(function(data){
+		showAllCourses(data);
+	})
+	.catch(error => console.log(error));
+}
+
+//function 1
+function showAllCourses(data)
+{
+	let table = document.getElementById('course_table');
+	table.innerHTML = '';
+
+	//for creating header of the table
+	let courses = data;
+	let tr = document.createElement('tr'),
+		th1 = document.createElement('th'),
+		th2 = document.createElement('th');
+	let th1text = document.createTextNode("Subject");
+	let th2text = document.createTextNode("Description");
+	th1.appendChild(th1text);
+	th2.appendChild(th2text);
+	tr.appendChild(th1);
+	tr.appendChild(th2);
+	table.appendChild(tr);
+
+	//for generating content of the table
+	for (i = 0; i < courses.length; i ++)
+	{
+		tr = document.createElement('tr');
+		th1 = document.createElement('th');
+		th2 = document.createElement('th');
+		let th1text = document.createTextNode(courses[i][0]);
+		let th2text = document.createTextNode(courses[i][1]);
+		th1.appendChild(th1text);
+		th2.appendChild(th2text);
+		tr.appendChild(th1);
+		tr.appendChild(th2);
+		table.appendChild(tr);
+	}
+}
+
+
+//fetch function 2
+function getCodesByGivenSubject()
+{
+	let subject = document.getElementById('subjectInputText').value;
+	fetch('http://localhost:3000/api/courses/' + subject)
+	.then((res) => res.json())
+	.then(function(data){
+		showCodesByGivenSubject(data);
+	})
+	.catch(error => alert('no code found for this subject'));
+}
+
+//function 2
+function showCodesByGivenSubject(data)
+{
+	let table = document.getElementById('course_table');
+	table.innerHTML = '';
+
+	//for creating header of the table
+	let tr = document.createElement('tr'),
+		th1 = document.createElement('th');
+	let th1text = document.createTextNode("Codes");
+	th1.appendChild(th1text);
+	tr.appendChild(th1);
+	table.appendChild(tr);
+
+	for (i = 0; i < data.length; i ++)
+	{
+		tr = document.createElement('tr');
+		th1 = document.createElement('th');
+		let th1text = document.createTextNode(data[i]);
+		th1.appendChild(th1text);
+		tr.appendChild(th1);
+		table.appendChild(tr);
+	}
+}
